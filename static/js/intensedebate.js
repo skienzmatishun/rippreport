ca80f8ffe0a134ba992ec965e825f1d473b51d02
skienzmatishun@aol.com <skienzmatishun@aol.com>
2023-04-03 22:28:35 -0500

    I forget

---
+++ static/js/intensedebate.js
@@ -0,0 +1,5 @@
+var idcomments_acct = '{{ .Site.IntenseDebateAccount }}';
+var idcomments_post_id;
+var idcomments_post_url;
+idcomments_post_id = {{ .Permalink }};
+idcomments_post_url = '{{ .Permalink }}';
