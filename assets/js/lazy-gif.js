abda9384bcccdb754f7a89b518df3c7214984941
skienzmatishun <skienzmatishun@aol.com>
2025-10-10 14:30:49 -0500

    idk

--- assets/js/lazy-gif.js
+++ assets/js/lazy-gif.js
@@ -2,17 +2,20 @@ function loadGifs() {
   const gifImages = document.querySelectorAll('.thumbnail__animated[data-src], .widget__animated[data-src]');
   
   gifImages.forEach(img => {
-    img.src = img.dataset.src;
-    img.removeAttribute('data-src');
-    
+    if (img.dataset.srcset) {
+      img.srcset = img.dataset.srcset;
+      img.removeAttribute('data-srcset');
+    }
+    if (img.dataset.src) {
+      img.src = img.dataset.src;
+      img.removeAttribute('data-src');
+    }
     img.onload = function() {
       img.classList.add('loaded');
     };
   });
 }
 
-// Wait for everything to finish loading before loading GIFs
 window.addEventListener('load', function() {
-  // Add a small delay to ensure all resources are truly loaded
   setTimeout(loadGifs, 100);
 });
