632c568cd5248a3a9af333a8b00d06b9429602ff
skienzmatishun <skienzmatishun@aol.com>
2025-12-11 19:17:41 -0600

    change logo

--- restore_archived_comments/restore_json_comments.sql
+++
@@ -1,8 +0,0 @@
-BEGIN TRANSACTION;
-INSERT INTO comments (page_id, author_name, content, content_hash, created_at, updated_at)
-VALUES ('/p/baldwin-county-sheriff-appointment/', 'Anonymous', 'Very interesting! Thanks for the insight. The BCSO has other scandals that haven’t been listed, such...', 'cd28c9127f8a2d24fac283471fd7246d', '2024-07-27T21:30:48.872000Z', '2024-07-27T21:30:48.872000Z');
-INSERT INTO comments (page_id, author_name, content, content_hash, created_at, updated_at)
-VALUES ('/p/baldwin-county-sheriff-appointment/', 'Anonymous', 'What about the story of Katherine Lee Ard. A woman from Elsanor that call the BCSO to have her abusi...', '4352eabbc22371dcfc4f3ab31ad43a47', '2024-07-27T22:34:13.024000Z', '2024-07-27T22:34:13.024000Z');
-INSERT INTO comments (page_id, author_name, content, content_hash, created_at, updated_at)
-VALUES ('/p/baldwin-county-sheriff-appointment/', 'Anonymous', 'Integrity. Where is the integrity that is so proudly boasted about within the department? We as citi...', 'ff4e1ecb78da67812875d68b8e944123', '2024-07-28T20:58:20.686000Z', '2024-07-28T20:58:20.686000Z');
-COMMIT;
